9df55873f5f0a24f5df2f4158ca400f43de9f9e1
ArseniyKorotin <reyziktv@gmail.com>
2023-09-17 13:08:06 +0300

    hometask_3.6

--- index.php
+++ index.php
@@ -1,25 +1,15 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <style>
-        table {
-  font-family: arial, sans-serif;
-  border-collapse: collapse;
-  width: 100%;
-}
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        
+    </head>
+    <body>
+    <form action="table.php" method="post">
+        <input type="text" id="a" name="a" placeholder="Введите n строчек"><br>
 
-td, th {
-  border: 1px solid #dddddd;
-  text-align: center;
-  padding: 8px;
-}
-    </style>
-</head>
-<body>
-  
-    <table><tr><th>X</th><th>Текст</th></tr><tr class="temp"><td><?php $x = rand(350, 420)/10; echo $x; ?></td><td><?php if($x < 37.7) { echo "Здоров!"; } elseif ($x > 37.7) { echo "Болен!"; } else { echo "Что-то не здоровится..."; } ?></td></tr></table>
-</body>
+        <input type="submit" placeholder="Отправить">
+    </form>
+    </body>
 </html>
---
+++ table.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+td, th {
+  border: 1px solid #dddddd;
+  text-align: center;
+  padding: 8px;
+}
+
+img {
+    width: 200px;
+    height: 200px;
+    object-fit: cover;
+}
+    </style>
+</head>
+<body>
+<table>
+  <tr>
+    <th>Номер</th>
+    <th>Картинки</th>
+  </tr>
+  <?php if ($_SERVER["REQUEST_METHOD"] === "POST") {$a = $_POST["a"];}
+for ($i = 1; $i <= $a; $i++) {
+    echo "<tr><td>{$i}</td><td><img src='./images/img_{$i}.jpg'></td></tr>";
+}?>
+</table>
+
+</body>
+</html>
